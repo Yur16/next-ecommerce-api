a485befe05a08805939962d567c12bd585cef58c
Yuri Oliveira <yurioliveira@aluno.fapce.edu.br>
2021-01-29 16:40:04 -0300

    feat: Populating database with web scrapping

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log('Starting populate...')
+    
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options)
+
+    ctx.send("Finished populating")
+  }
+};
